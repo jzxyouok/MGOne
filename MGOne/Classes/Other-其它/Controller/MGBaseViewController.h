46adc6dd844acc0dad2a3a074839857fd8fcc433
angmu <anglimu@163.com>
2016-05-09 04:09:00 +0800

    集成cocoaPods

---
+++ MGOne/Classes/Other-其它/Controller/MGBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGBaseViewController.h
+//  MGOne
+//
+//  Created by 穆良 on 16/5/9.
+//  Copyright © 2016年 穆良. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGBaseViewController : UIViewController
+
+@end
---
+++ MGOne/Classes/Other-其它/Controller/MGBaseViewController.m
@@ -0,0 +1,33 @@
+//
+//  MGBaseViewController.m
+//  MGOne
+//
+//  Created by 穆良 on 16/5/9.
+//  Copyright © 2016年 穆良. All rights reserved.
+//
+
+#import "MGBaseViewController.h"
+
+@interface MGBaseViewController ()
+
+@end
+
+@implementation MGBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
